ba7ede093fa010e49b323e7928f92d7bf2d2f3a1
Sagnik Saha <sagnik12347@gmail.com>
2024-12-01 13:12:49 +0530

    first commit

---
+++ app/(tabs)/bookmarks.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const bookmarks = () => {
+  return (
+    <View>
+      <Text>bookmarks</Text>
+    </View>
+  )
+}
+
+export default bookmarks
+
+const styles = StyleSheet.create({})
